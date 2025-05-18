00c42ae0e96f4c56ec602cb020fdf3226b3389f5
rhm0202 <83155164+rhm0202@users.noreply.github.com>
2025-05-19 01:47:56 +0900

    .
    .

--- Assets/Script/PlayerAction.cs
+++ Assets/Script/PlayerAction.cs
@@ -2,37 +2,53 @@ using UnityEngine;
 
 public class PlayerAction : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float speed = 5f;
     public float jumpForce = 7f;
     public Transform groundCheck;
-    public float groundCheckRadius = 0.2f;
     public LayerMask groundLayer;
 
     private Rigidbody2D rb;
+    private Animator animator;
     private bool isGrounded;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         float moveInput = Input.GetAxisRaw("Horizontal");
-        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
 
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocity.y);
+
+        animator.SetBool("Move", true);
+
+        if (moveInput == 0)
+        {
+            animator.SetBool("Move", false);
+        }
+
+            // 좌우 바라보는 방향 바꾸기
+        if (moveInput != 0)
+        {
+            Vector3 scale = transform.localScale;
+            scale.x = moveInput > 0 ? 1 : -1;
+            transform.localScale = scale;
+        }
+
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
 
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
         }
-    }
 
-    void OnDrawGizmosSelected()
-    {
+        animator.SetBool("IsJumping", !isGrounded);
 
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+        bool crouching = Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow);
+        animator.SetBool("IsCrouching", crouching);
     }
 }
