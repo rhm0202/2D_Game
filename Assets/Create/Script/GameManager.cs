cb77a27fa607c0d252d843f60d2a4a5d0b4523b7
rhm0202 <83155164+rhm0202@users.noreply.github.com>
2025-05-20 15:53:08 +0900

    .
    .

---
+++ Assets/Create/Script/GameManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
